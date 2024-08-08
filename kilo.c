1c9f071d3089ac051d1ed14c5f7c2a23e0b9adb5
Simon Gotz <simongotz@msn.com>
2024-08-08 20:11:40 +0200

    updatess

--- kilo.c
+++ kilo.c
@@ -69,7 +69,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void(*callback)(char *, int));
 
 /*** terminal ***/
 
@@ -184,6 +184,19 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /*** row operations ***/
+
+int editorRowRxToCx(erow *row, int rx){
+	int cur_rx = 0;
+	int cx;
+	for(cx = 0; cx < row->size; cx++){
+		if(row->chars[cx] == '\t')
+			cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+		cur_rx++;
+		
+		if (cur_rx > rx) return cx;
+	}
+	return cx;
+}
 
 int editorRowCxToRx(erow *row, int cx){
 	int j;
@@ -365,7 +378,7 @@ void editorOpen(char *filename) {
 
 void editorSave(){
 	if (E.filename == NULL) {
-		E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+		E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
 		if (E.filename == NULL) return;
 	}
 	
@@ -388,6 +401,62 @@ void editorSave(){
 	}
 	free(buf);
 	editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
+/*** find ***/
+
+void editorFindCallback(char *query, int key){
+	static int last_match = -1;
+	static int direction = 1;
+	
+	if (key == '\r' || key == '\x1b'){
+		last_match = -1;
+		direction = 1;
+		return;
+	} else if (key == ARROW_RIGHT || key == ARROW_DOWN){
+		direction = 1;
+	} else if (key == ARROW_LEFT || key == ARROW_UP){
+		direction = -1;
+	} else {
+		last_match = -1;
+		direction = 1;
+	}
+	
+	if (last_match == -1) direction = 1;
+	int current = last_match;
+	
+	int i;
+	for (i = 0; i < E.numrows; i++){
+		current += direction;
+		if (current == -1) current = E.numrows - 1;
+		else if (current == E.numrows) current = 0;
+		
+		erow *row = &E.row[i];
+		char *match = strstr(row->render, query);
+		if (match) {
+			last_match = current;
+			E.cy = current;
+			E.cx = editorRowRxToCx(row, match - row->render);
+			E.rowoff = E.numrows;
+			break;
+		}
+	}
+}
+
+void editorFind(){
+	int saved_cx = E.cx;
+	int saved_cy = E.cy;
+	int saved_coloff = E.coloff;
+	int saved_rowoff = E.rowoff;
+	
+	char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
+	if (query) free(query);
+	else {
+		E.cx = saved_cx;
+		E.cy = saved_cy;
+		E.coloff = saved_coloff;
+		E.rowoff = saved_rowoff;
+	}
 }
 
 /*** append buffer ***/
@@ -584,6 +653,10 @@ void editorProcessKeypress() {
 		editorInsertNewLine();
 		break;
 	
+	case CTRL_KEY('f'):
+		editorFind();
+		break;
+	
 	case CTRL_KEY('s'):
 		editorSave();
 		break;
@@ -651,7 +724,7 @@ void editorProcessKeypress() {
   quit_times = KILO_QUIT_TIMES;
 }
 
-char *editorPrompt(char *prompt){
+char *editorPrompt(char *prompt, void (*callback)(char *, int)){
 	size_t bufsize = 128;
 	char *buf = malloc(bufsize);
 	
@@ -669,12 +742,14 @@ char *editorPrompt(char *prompt){
 			editorDelChar();
 		} else if( c == '\x1b'){
 			editorSetStatusMessage("");
+			if (callback) callback(buf, c);
 			free(buf);
 			return NULL;
 		}
 		else if (c == '\r'){
 			if (buflen != 0){
 				editorSetStatusMessage("");
+				if (callback) callback(buf, c);
 				return buf;
 			}
 		} else if (!iscntrl(c) && c < 128){
@@ -685,6 +760,8 @@ char *editorPrompt(char *prompt){
 			buf[buflen++] = c;
 			buf[buflen] = '\0';
 		}
+		
+		if (callback) callback(buf, c);
 	}
 }
 /*** init ***/
@@ -713,7 +790,7 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
   
-  editorSetStatusMessage("HELP: Ctrl-Q = quit | Ctrl-S = save");
+  editorSetStatusMessage("HELP: Ctrl-Q = quit | Ctrl-S = save | Ctrl-f = find");
 
   while (1) {
     editorRefreshScreen();
